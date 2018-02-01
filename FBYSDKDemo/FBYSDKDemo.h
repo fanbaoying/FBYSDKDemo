2348c4db6b141a058184b3c1f5011064f3e837cc
BaoYing-Fan <1479162730@qq.com>
2018-02-01 11:49:49 +0800

    静态库生成demo

--- FBYSDKDemo/FBYSDKDemo.h
+++ FBYSDKDemo/FBYSDKDemo.h
@@ -2,12 +2,25 @@
 //  FBYSDKDemo.h
 //  FBYSDKDemo
 //
-//  Created by 范保莹 on 2018/1/31.
+//  Created by fby on 2018/1/31.
 //  Copyright © 2018年 FBYSDKDemo. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
+typedef void (^FBYSDKCompletion)(NSString *result);
+
 @interface FBYSDKDemo : NSObject
+
+/**
+ *
+ *  @param urltype       网页类型信息 urltype可为iOS、Android
+ *  @param completion    根据urltype获取到相应网页url结果回调
+ *
+ */
+
++ (void)urlType:(NSString *)urltype withCompletion:(FBYSDKCompletion)completion;
+
 
 @end
--- FBYSDKDemo/FBYSDKDemo.m
+++ FBYSDKDemo/FBYSDKDemo.m
@@ -2,12 +2,30 @@
 //  FBYSDKDemo.m
 //  FBYSDKDemo
 //
-//  Created by 范保莹 on 2018/1/31.
+//  Created by fby on 2018/1/31.
 //  Copyright © 2018年 FBYSDKDemo. All rights reserved.
 //
 
 #import "FBYSDKDemo.h"
 
 @implementation FBYSDKDemo
+
++ (void)urlType:(NSString *)urltype withCompletion:(FBYSDKCompletion)completion{
+    
+    if ([urltype isEqualToString:[NSString stringWithFormat:@"iOS"]]) {
+        
+        if (completion) {
+            completion(@"https://juejin.im/post/5a41c04c6fb9a044fc44fd23");
+        }
+        
+    }else if ([urltype isEqualToString:[NSString stringWithFormat:@"Android"]]) {
+        
+        if (completion) {
+            completion(@"https://juejin.im/post/5a31e6adf265da430c11d41f");
+        }
+        
+    }
+    
+}
 
 @end
